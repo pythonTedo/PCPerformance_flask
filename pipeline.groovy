6c576d3b0cf50407ffd848f19ad7387f2e675564
Teodor Chakarov <tedochakarov@gmial.com>
2025-01-02 08:17:28 +0000

    update pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,15 +1,33 @@
 pipeline {
     agent { label 'host-agent' }
     environment {
-        PYTHON_VERSION = "3.11"
         FLASK_ENV = "developement"
         FLASK_APP = "src/app.py"
+        SRC_DIR = "src"
     }
     stages {
-        stage('Check if python exists') {
+        stage('Check if python exists and docker exists') {
             steps {
                 sh 'python3.11 --version'
-                sh 'docker ps'
+            }
+        }
+        stage('Prepare venv') {
+            steps {
+                script {
+                    sh 'python3.11 -m venv ${SRC_DIR}/venv'
+                    sh 'source venv/bin/activate'
+                    sh 'pip install -r requirements.txt'
+                }
+            }
+        }
+        stage('Run tests') {
+            steps {
+                sh 'make test'
+            }
+        }
+        stage('Clean environment') {
+            steps {
+                sh 'deactivate; rm -rf ${SRC_DIR}/venv'
             }
         }
     }
